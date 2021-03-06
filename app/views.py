e60dace3c37c977052d71b5b35b4b7ee15facd10
Francisco <victoriafrancis2009@gmail.com>
2021-03-06 13:03:26 -0300

    pruebas reporte

--- app/views.py
+++ app/views.py
@@ -5,9 +5,12 @@ from django.views import generic
 from django.utils import timezone
 from django.urls import reverse, reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from reportlab.lib import colors
 from .models import *
 from .forms import *
 from reportlab.pdfgen import canvas
+from reportlab.platypus import Table, TableStyle
+from reportlab.lib.pagesizes import letter, A4
 
 def index(request):
 	cliente = Cliente.objects.get(id=1)
@@ -92,21 +95,42 @@ def allCharts(request):
 	return render(request, 'app/chart.html', context)
 
 def pdf_test(request, pk):
+	#queries for models
+	mediciones = Medicion.objects.filter(pila__id=pk)
+	pila = Pila.objects.get(id=pk)
 	# Create a file-like buffer to receive PDF data.
-    buffer = io.BytesIO()
+	buffer = io.BytesIO()
 
-    # Create the PDF object, using the buffer as its "file."
-    p = canvas.Canvas(buffer)
+	# Create the PDF object, using the buffer as its "file."
+	p = canvas.Canvas(buffer, pagesize=letter)
+	title = f'Reporte Pila {pila.nombreID}'
+	p.setTitle(title)
 
-    # Draw things on the PDF. Here's where the PDF generation happens.
-    # See the ReportLab documentation for the full list of functionality.
-    p.drawString(100, 100, "Hello world.")
+	width, height = letter 
+	#tabla
+	#data = [['1','2','3'],
+	#		['4','5','6']]
+	#t = Table(data)
+	#t.setStyle(TableStyle([('BACKGROUND', (1,1), (-2,-2), colors.green)]))
+	#w, h = t.wrapOn(p,400,100)
+	
+	# Draw things on the PDF. Here's where the PDF generation happens.
+	# See the ReportLab documentation for the full list of functionality.
+	p.drawString(100, 100, "Hello world.")
+	p.drawCentredString(100, height-10, title)
+
+	lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris commodo rutrum libero eget posuere. Pellentesque ornare dignissim ante ut finibus. Curabitur id massa sit amet elit sagittis euismod sed sit amet ex. Morbi mauris felis, pulvinar eget est in, sodales bibendum quam. Aenean in arcu et arcu consectetur rutrum. Aliquam tellus arcu, auctor eu lacus sed, tempus accumsan tortor. Etiam rutrum, ante quis condimentum laoreet, nunc sapien accumsan nibh, sit amet aliquet dui ligula tincidunt dui. Ut pretium, massa nec egestas maximus, mauris leo sagittis diam, ac ullamcorper mi odio a ipsum. Vivamus imperdiet pharetra tellus, ac porttitor magna ornare nec. Nulla facilisi. Interdum et malesuada fames ac ante ipsum primis in faucibus."
+
+	p.drawCentredString(100,700, lorem)
+
 
     # Close the PDF object cleanly, and we're done.
-    p.showPage()
-    p.save()
+	p.showPage()
+	p.save()
 
-    # FileResponse sets the Content-Disposition header so that browsers
-    # present the option to save the file.
-    buffer.seek(0)
-    return FileResponse(buffer, as_attachment=False, filename='hello.pdf')
+	# FileResponse sets the Content-Disposition header so that browsers
+	# present the option to save the file.
+	buffer.seek(0)
+	#nombre de reporte
+	
+	return FileResponse(buffer, as_attachment=False, filename='reporte.pdf')
