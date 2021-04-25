2fca94cdcfde19b574f66c33ca872f98d183efb9
francisco <ffloresm@udec.cl>
2021-04-25 01:57:29 -0400

    get temp

--- thermsensor_lcd.py
+++ thermsensor_lcd.py
@@ -58,14 +58,13 @@ def showPilas(predio):
 			i=0
 
 try:
+	display.lcd_display_string("BIENVENIDO", 1)
+	display.lcd_display_string("v. 1.0.1", 2)
+	time.sleep(2)
 	display.lcd_clear()
 	while True:
-		#if select_button.is_pressed:
-		#	display.lcd_display_string("BOTON 1!!",1)
 		temp = sensor.get_temperature() #temp en celcius
 		print("Temperatura en celcius", temp)
-		display.lcd_display_string("BIENVENIDO", 1)
-		display.lcd_display_string("v. 1.0.1", 2)
 		time.sleep(2)
 		showPredios()
 		time.sleep(2)
@@ -73,10 +72,14 @@ try:
 		showPilas(predio_selected)
 		display.lcd_clear()
 		print("OK")
-		#display.lcd_display_string(f"temp. 11.4 {chr(223)}C", 1)
-		#display.lcd_display_string(f"{temp:.2f} {chr(223)}C", 2)
-		#display.lcd_display_string("humedad 57 %", 2)
-		#time.sleep(10)
+		#display.lcd_display_string(f"temp. {temp}  {chr(223)}C", 1)
+		display.lcd_display_string(f"temp. {temp:.2f} {chr(223)}C", 1)
+		display.lcd_display_string("humedad 57 %", 2)
+		time.sleep(5)
+		#TOMAR FOTO
+		#OBTENER POSICION
+		#GUARDAR EN BD LOCAL
+		#ENVIAR POR REST A REMOTO (solo si hay interneT)
 		#display.lcd_clear()
 		#display.lcd_display_string("DATOS", 1)
 		#display.lcd_display_string("GUARDADOS", 2)
@@ -84,7 +87,7 @@ try:
 		#display.lcd_clear()
 #time.sleep(1)
 		#display.lcd_clear()
-
+		display.lcd_clear()
 except KeyboardInterrupt:
 	print("Terminado!")
 	display.lcd_clear()
